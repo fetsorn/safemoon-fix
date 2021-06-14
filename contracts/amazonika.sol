f7ad75f737b5ac525ce4eb2ab1fe19e23d56206f
Anton Davydov <fetsorn@gmail.com>
2021-06-14 13:20:34 +0300

    fix: fix SSL-02 redundant code

--- contracts/amazonika.sol
+++ contracts/amazonika.sol
@@ -1155,8 +1155,6 @@ contract SafeMoon is Context, IERC20, Ownable {
             _transferFromExcluded(sender, recipient, amount);
         } else if (!_isExcluded[sender] && _isExcluded[recipient]) {
             _transferToExcluded(sender, recipient, amount);
-        } else if (!_isExcluded[sender] && !_isExcluded[recipient]) {
-            _transferStandard(sender, recipient, amount);
         } else if (_isExcluded[sender] && _isExcluded[recipient]) {
             _transferBothExcluded(sender, recipient, amount);
         } else {
