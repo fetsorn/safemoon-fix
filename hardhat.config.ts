fb4facbebc6e77573dd615c0272e878bae46bdb1
Anton Davydov <fetsorn@gmail.com>
2021-06-14 13:06:35 +0300

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,5 @@
+import "@typechain/hardhat";
+
+export default {
+  solidity: "0.6.12",
+};
---
+++ test/amazonika.spec.ts
@@ -0,0 +1,24 @@
+import { ethers, waffle } from 'hardhat'
+import { SafeMoon } from '../typechain/SafeMoon'
+
+describe('Amazonika', () => {
+  const [wallet, other] = waffle.provider.getWallets()
+
+  let safemoon: SafeMoon
+
+  const fixture = async () => {
+    const safemoonFactory = await ethers.getContractFactory('SafeMoon')
+    return (await safemoonFactory.deploy()) as SafeMoon
+  }
+
+  let loadFixture: ReturnType<typeof createFixtureLoader>
+
+  before('create fixture loader', async () => {
+    loadFixture = createFixtureLoader([wallet, other])
+  })
+
+  beforeEach('deploy safemoon', async () => {
+    safemoon = await loadFixture(fixture)
+  })
+
+})
