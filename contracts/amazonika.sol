31f5125df18b868391e58b7b40fdfb1c259e2ea6
Anton Davydov <fetsorn@gmail.com>
2021-06-14 14:26:21 +0300

    fix: remove unused variable

--- contracts/amazonika.sol
+++ contracts/amazonika.sol
@@ -1161,7 +1161,7 @@ contract SafeMoon is Context, IERC20, Ownable {
     }
 
     function _transferStandard(address sender, address recipient, uint256 tAmount) private {
-        (uint256 rAmount, uint256 rTransferAmount, uint256 rFee, uint256 tTransferAmount, uint256 tFee, uint256 tLiquidity) = _getValues(tAmount);
+        (uint256 rAmount, uint256 rTransferAmount, , uint256 tTransferAmount, uint256 tFee, uint256 tLiquidity) = _getValues(tAmount);
         _rOwned[sender] = _rOwned[sender].sub(rAmount);
         _rOwned[recipient] = _rOwned[recipient].add(rTransferAmount);
         _takeLiquidity(tLiquidity);
